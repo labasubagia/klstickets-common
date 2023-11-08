04f621ac5a5e095a8c07444e6b8dfd069d31d13b
Laba Subagia <labasubagia22@gmail.com>
2023-11-09 03:19:31 +0800

    chore: update

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,15 +1,33 @@
 export interface Event {
-  topic: Topic;
-  subject: Subject;
-  data: object;
+  topic: Topic
+  subject: Subject
+  data: object
 }
 
 export enum Topic {
-  Ticket = "ticket",
+  Ticket = 'ticket'
 }
 
 export enum Subject {
-  TicketCreated = "ticket.created",
-  TicketUpdated = "ticket.updated",
-  OrderUpdated = "order.updated",
+  TicketCreated = 'ticket.created',
+  TicketUpdated = 'ticket.updated',
+  OrderUpdated = 'order.updated'
+}
+
+// Order
+export enum OrderStatus {
+  // when the order has been created, but the ticket
+  // is trying to order has not been reserved
+  Created = 'created',
+
+  // the ticket the order trying to reserve has already been reserved
+  // or when the user has cancelled the order
+  // or the order expires before payment
+  Cancelled = 'cancelled',
+
+  // the order has successfully reserved the ticket
+  AwaitingPayment = 'awaiting:payment',
+
+  // the order has reserved and the user has provided payment successfully
+  Complete = 'complete'
 }
